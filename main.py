cf7b90f3dfcb7efc4b8d7960058f20a222387b6c
bisre1921 <bisrattewodros3@gmail.com>
2025-05-18 16:54:41 +0300

    enable CORS policy

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from routes import Customer_forecast,items_forecast
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(Customer_forecast.router, prefix="/customer", tags=["customer"])
 app.include_router(items_forecast.router, prefix="/items", tags=["items-forecast"])
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
